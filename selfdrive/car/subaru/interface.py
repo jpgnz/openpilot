49c6815842d39765149234fafc975691911ac557
James <james@grhm.nz>
2022-04-03 08:07:34 +1200

    Apply Outback tunables to Levorg

--- selfdrive/car/subaru/interface.py
+++ selfdrive/car/subaru/interface.py
@@ -141,11 +141,11 @@ class CarInterface(CarInterfaceBase):
        ret.mass = 1568 + STD_CARGO_KG
        ret.wheelbase = 2.67
        ret.centerToFront = ret.wheelbase * 0.5
-       ret.steerRatio = 12.5   # 14.5 stock
-       ret.steerActuatorDelay = 0.15
-       ret.lateralTuning.pid.kf = 0.00005
-       ret.lateralTuning.pid.kiBP, ret.lateralTuning.pid.kpBP = [[0., 20.], [0., 20.]]
-       ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.1, 0.2], [0.01, 0.02]]
+       ret.steerRatio = 20   # 14.5 stock
+       ret.steerActuatorDelay = 0.1
+       ret.lateralTuning.pid.kf = 0.000039
+       ret.lateralTuning.pid.kiBP, ret.lateralTuning.pid.kpBP = [[0., 10., 20.], [0., 10., 20.]]
+       ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.01, 0.05, 0.2], [0.003, 0.018, 0.025]]
 
     if candidate == CAR.OUTBACK_PREGLOBAL:
       ret.mass = 1568 + STD_CARGO_KG
